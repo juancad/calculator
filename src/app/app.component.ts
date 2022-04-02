a04d2491fe79d988e821c53e04550d1ffcd112f0
juancad <juancarlosalcd@gmail.com>
2022-04-02 12:41:36 +0100

    add functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,7 +33,21 @@ export class AppComponent {
     this.result=((Math.sqrt(this.num1)));
   }
   pow():void{
-    this.textResult=((this.num1+"^"+this.num2+"= "));
+    this.textResult=((this.num1+"<sup>"+this.num2+"</sup>= "));
     this.result=((Math.pow(this.num1, this.num2)));
+  }
+  log():void{
+    this.textResult=(("log<sub>"+this.num2+"</sub>"+this.num1+"= "));
+    this.result=((Math.log(this.num1)/Math.log(this.num2)));
+  }
+  fact():void{
+    this.textResult=((this.num1+"!"+"= "));
+    this.result=((this.factorial(this.num1)));
+  }
+
+  factorial(x: number): number {
+    if(x==0)
+      return 1;
+    return x * this.factorial(x-1);
   }
 }
